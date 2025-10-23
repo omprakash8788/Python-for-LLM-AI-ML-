0be17f6e2abefb64fed5792438873a4269168f10
omprakash8788 <omprakashkumar552@gmail.com>
2025-10-24 02:09:46 +0530

    chap1 and chap2

---
+++ ch2/data_type.py
@@ -0,0 +1,23 @@
+# Variable and simple data types
+
+# In this chapter you’ll learn
+# about the different kinds of
+# data you can work with in your
+# Python programs. You’ll also
+# learn how to use variables to
+# represent data in your
+# programs.
+
+message="Hello op , how are you"
+print(message)
+
+# To run in terminal type this " python data_type.py"
+
+# Explain - >>> message --> It is a variable name.
+# value --->> "Hello op , how are you"
+
+message="Hello Python Crash Course world!"
+print(message)
+
+# Attention --> same var we use two times
+
