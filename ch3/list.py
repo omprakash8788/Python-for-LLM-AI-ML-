84a780fb3d19a402325bee0a93d2c30d103f112d
omprakash8788 <omprakashkumar552@gmail.com>
2025-10-24 20:12:49 +0530

    chapter 3 done

---
+++ ch3/list.py
@@ -0,0 +1,63 @@
+# bicyles=['trek', 'cannonalde', 'redline', 'specialize']
+# print(bicyles)
+
+# Access the ele by index number
+# bicyles=['trek', 'cannonalde', 'redline', 'specialize']
+# print(bicyles[0])
+
+
+# bicyles=['trek', 'cannonalde', 'redline', 'specialize']
+# message=f"My first bicyle was a {bicyles[0].title()}"  # This is the way we get data in dynamic nature 
+# print(message)
+
+
+#  Modifying, Adding, and Removing
+#  Elements
+
+
+#Modifying
+# motercycle=['honda', 'yamaha', 'suzuki']
+# print(motercycle)
+# motercycle[0]='ducati'
+# print(motercycle)
+
+
+# Append
+
+# motercycle=['honda', 'yamaha', 'suzuki']
+# motercycle.append('Hero')
+# print(motercycle)
+
+
+# Inserting
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# motercycle.insert(0, 'ducati')
+# print(motercycle)
+
+
+#  Removing Elements from a List
+# del --> used this to delete it
+# motorcycles=['honda', 'yamaha', 'suzuki']
+# del motorcycles[0]
+# print(motorcycles)
+
+
+#  Removing an Item by Value
+# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+# motorcycles.remove("suzuki")
+# print(motorcycles)
+
+
+# Printing a List in Reverse Order
+# cars = ['bmw', 'audi', 'toyota', 'subaru'] 
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+# Find the length
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
+
+
+
+
