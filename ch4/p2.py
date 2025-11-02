4103dda3241f84949b9f11cf359f2f9d946de07c
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2025-11-02 10:21:12 +0530

    Update p2.py

--- ch4/p2.py
+++ ch4/p2.py
@@ -2,6 +2,7 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
  print(f"{magician.title()}, that was a great trick!")
 
+ 
 # Output 
 #  Alice, that was a great trick!
 # David, that was a great trick!
